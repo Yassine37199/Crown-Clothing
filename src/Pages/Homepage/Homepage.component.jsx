b8226cbda47042a39318f466bf7f335edfbc67fe
Yassine37199 <yassine.ouri7319@gmail.com>
2021-02-16 11:37:30 +0100

    init project

---
+++ src/Pages/Homepage/Homepage.component.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import './Homepage.styles.scss'
+
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">PUZZLE </h1>
+                    <span className="subtitle">DISCOVER NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">ACTION</h1>
+                    <span className="subtitle">DISCOVER NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">ADVENTURE </h1>
+                    <span className="subtitle">DISCOVER NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">RACING </h1>
+                    <span className="subtitle">DISCOVER NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Multiplayer</h1>
+                    <span className="subtitle">DISCOVER NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
