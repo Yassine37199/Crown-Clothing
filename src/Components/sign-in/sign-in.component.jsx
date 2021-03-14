bd9e86d9eda29e102a2d675361281f26b8ab3b7e
Yassine37199 <yassine.ouri7319@gmail.com>
2021-03-14 21:38:41 +0100

    init Redux

--- src/Components/sign-in/sign-in.component.jsx
+++ src/Components/sign-in/sign-in.component.jsx
@@ -69,7 +69,7 @@ class SignIn extends Component {
 
                     <div className="button-wrapper">
                        <CustomButton type="submit" value="Submit Form"> Sign In</CustomButton>
-                        <CustomButton onClick={SignInWithGoogle} GoogleSignIn> Sign In With Google</CustomButton>
+                        <CustomButton type="button" nClick={SignInWithGoogle} GoogleSignIn> Sign In With Google</CustomButton>
                     </div> 
                 </form>
             </div> 
